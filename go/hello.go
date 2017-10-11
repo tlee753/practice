2a0ac2ad57eedcd44833085a3adf26da38274059
Tyler Lee <Tyler@Tylers-MacBook-Pro-3.local>
2017-10-10 23:57:09 -0400

    messing around

--- go/hello.go
+++ go/hello.go
@@ -4,11 +4,15 @@ import "fmt"
 
 func main() {
 	fmt.Println("hello world")
-	fmt.Println(minMax(2, 3))
+	a := 3
+	b := 2
+	a, b = minMax(a, b)
+	fmt.Printf("Min is %d.\n", a)
+	fmt.Printf("Max is %d.\n", b)
 }
 
 func minMax(a, b int) (int, int) {
-	if (a >= b) {
+	if a >= b {
 		return b, a
 	}
 	return a, b
