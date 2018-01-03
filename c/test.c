03f2d34b9ba87784a060d61dcb358d109d8174bf
Tyler Lee <tlee753@gmail.com>
2018-01-03 10:29:26 -0500

    updates

--- c/test.c
+++ c/test.c
@@ -6,6 +6,7 @@ int A[3];         // int array
 char s[] = "Hi!"; // string
 
 int main() {
-    printf("%d\n", i); // type double
-    return 0;          // main method expects an int to return, not really neccessary for main
+  printf("%d\n", i); // type double
+  return 0; // main method expects an int to return, not really neccessary for
+            // main
 }
